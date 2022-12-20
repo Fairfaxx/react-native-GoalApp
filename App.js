db0ab3c68df3654693eb2d741ae8a361d5e24815
FefeLemaire <federico.lemaire@alloy.digital>
2022-12-19 21:35:09 -0300

    adding data to the list done

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
+  const [goal, setGoal] = useState('');
+  const [goalsList, setGoalsList] = useState([]);
+
+  function inputHandler(event) {
+    setGoal(event);
+  }
+
+  const handleSubmit = () => {
+    setGoalsList((currentGoalList) => [...currentGoalList, goal]);
+    setGoal('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.textInput}
+          onChangeText={inputHandler}
+        />
+        <Button onPress={handleSubmit} title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        {goalsList?.map((g, i) => (
+          <Text key={i}>{g}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
